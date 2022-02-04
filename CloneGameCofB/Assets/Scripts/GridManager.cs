18854c7140d49ba8358ea4d46346098f0c4d2619
cankcl2 <80987102+cankcl2@users.noreply.github.com>
2022-02-05 01:59:38 +0300

    Created spread funcs
    123

--- CloneGameCofB/Assets/Scripts/GridManager.cs
+++ CloneGameCofB/Assets/Scripts/GridManager.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GridManager : MonoBehaviour
 {
@@ -17,6 +19,13 @@ public class GridManager : MonoBehaviour
     {0,0,0,0,0,0,0},
     {0,0,0,0,0,0,0}
     };
+    public enum OyuncuTuru
+    {
+        Dusman,
+        Ben,
+        Doldurulamayan,
+        Bos
+    }
 
     void Start()
     {
@@ -24,33 +33,73 @@ public class GridManager : MonoBehaviour
     }
     IEnumerator PlusDirections()
     {
-
+        int o = 0;
         while (true)
         {
+            o++;
+            yield return new WaitForSeconds(0.5f);
+            InstantiateAll(grid);
+            var newGrid = grid.Clone() as int[,];
+
             for (int i = 0; i < 9; i++)
             {
                 for (int j = 0; j < 7; j++)
                 {
                     if (grid[i, j] == 1)
                     {
-                        yield return new WaitForSeconds(0.25f);
-                        Instantiate(_enemyPrefab, new Vector3(i * 10, 0, j * 10), Quaternion.identity);
-                        if (i > 0 && i < 8 )
-                        {
-                            grid[i + 1, j] = 1;
-                        }
-                        if(j > 0 && j < 6) 
-                        { 
-                            grid[i, j + 1] = 1;
-                        }
-                        
 
+                        if (i > 0)
+                        {
+                            if(newGrid[i - 1, j]==0)
+                                newGrid[i - 1, j] = 1;
+
+                        }
+                        if (i < 8)
+                        {
+                            if (newGrid[i + 1, j] == 0)
+                                newGrid[i + 1, j] = 1;
+
+                        }
+                        if (j > 0)
+                        {
+                            if (newGrid[i, j - 1] == 0)
+                                newGrid[i, j - 1] = 1;
+                        }
+                        if (j < 6)
+                        {
+                            if (newGrid[i, j + 1] ==0)
+                                newGrid[i, j + 1] = 1;
+                        }
                     }
                 }
             }
-            break;
+            grid = newGrid;
+            if (o>20)
+                break;
 
         }
     }
 
+    private void InstantiateAll(int[,] grid)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 7; j++)
+            {
+                if (grid[i, j] == 1)
+                {
+                    Instantiate(_enemyPrefab, new Vector3(i * 10, 0, j * 10), Quaternion.identity);
+                }
+                if (grid[i, j] == 2)
+                {
+                    //Instantiate(_enemyPrefab, new Vector3(i * 10, 0, j * 10), Quaternion.identity);
+                }
+            }
+        }
+
+    }
+    public void OnMouseDown()
+    {
+        SceneManager.LoadScene("Level6");
+    }
 }
